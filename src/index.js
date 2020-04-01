931dfcec1bdee4489dbb93abce86e67736ee0411
Sam Brown <sam@sgb.io>
2020-04-01 21:09:12 +0100

    Single fields parsed and cleaned

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const blocks = WorldTourRaw.toString().split(
 );
 
 const nl = "(\r?\n|\r)";
-const roundMatcher = new RegExp(`^(.+)${nl}(=+)${nl}$`, "gim");
+const roundMatcher = new RegExp(
+  `^(.+)${nl}(=+)${nl}$`,
+  "gim"
+);
 const missionNameLine = "\\s{3}\\+\\+\\+\\s(.+)";
 const missionNameMatcher = new RegExp(
   `^(.+)${nl}${missionNameLine}${nl}$`,
@@ -16,16 +19,26 @@ const contentBlockMatcher = new RegExp(
   `^(.+)${nl}(${conditionLine})+${nl}${nl}$`,
   "gim"
 );
-const oppositionNameMatcher = new RegExp("^My score:\\s(.+)\\s-\\s", "gim");
+const oppositionNameMatcher = new RegExp(
+  "^My score:\\s(.+)\\s-\\s",
+  "gim"
+);
 
 let missions = [];
 let round = undefined;
 
+const removeNewlines = string =>
+  string.replace("\r", "").replace("\n", "");
+
 for (let i = 0; i < blocks.length; i += 1) {
   const roundName = blocks[i].match(roundMatcher);
   const missionName = blocks[i].match(missionNameMatcher);
-  const contentBlocks = blocks[i].match(contentBlockMatcher);
-  const oppositionName = blocks[i].match(oppositionNameMatcher);
+  const contentBlocks = blocks[i].match(
+    contentBlockMatcher
+  );
+  const oppositionName = blocks[i].match(
+    oppositionNameMatcher
+  );
 
   if (!roundName && !round) {
     throw new Error("Round not found - unable to continue");
@@ -33,18 +46,33 @@ for (let i = 0; i < blocks.length; i += 1) {
 
   // New round
   if (roundName) {
-    round = roundName[0]
+    round = removeNewlines(roundName[0])
       .trim()
-      .replace("\r", "")
-      .replace("\n", "")
       .replace(/=/g, "");
   }
 
+  const cleanMissionName =
+    missionName &&
+    removeNewlines(missionName[0])
+      .trim()
+      .replace(/^Mission\s\d(\d*)\s\s\s/g, "")
+      .replace(/\+\+\+/g, "")
+      .trim();
+
+  const cleanOppositionName =
+    oppositionName &&
+    removeNewlines(oppositionName[0])
+      .trim()
+      .replace("My score: ", "")
+      .replace(/\s-(.+)$/g, "")
+      .trim();
+  // console.log(cleanOppositionName);
+
   missions.push({
     roundName: round,
-    missionName,
+    missionName: cleanMissionName,
     contentBlocks,
-    oppositionName
+    oppositionName: cleanOppositionName
   });
 }
 
