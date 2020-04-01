ea18d42cda7f2a7f84605a07abc80960f93b2648
Sam Brown <sam@sgb.io>
2020-04-01 17:16:38 +0100

    Implement various matchers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,52 @@
-import "./styles.css";
+import WorldTourRaw from "./world_tour.txt";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const blocks = WorldTourRaw.toString().split(
+  "----------------------------------"
+);
+
+const nl = "(\r?\n|\r)";
+const roundMatcher = new RegExp(`^(.+)${nl}(=+)${nl}$`, "gim");
+const missionNameLine = "\\s{3}\\+\\+\\+\\s(.+)";
+const missionNameMatcher = new RegExp(
+  `^(.+)${nl}${missionNameLine}${nl}$`,
+  "gim"
+);
+const conditionLine = "\\s{3}\\d.\\s(.+)";
+const contentBlockMatcher = new RegExp(
+  `^(.+)${nl}(${conditionLine})+${nl}${nl}$`,
+  "gim"
+);
+const oppositionNameMatcher = new RegExp("^My score:\\s(.+)\\s-\\s", "gim");
+
+let missions = [];
+let round = undefined;
+
+for (let i = 0; i < blocks.length; i += 1) {
+  const roundName = blocks[i].match(roundMatcher);
+  const missionName = blocks[i].match(missionNameMatcher);
+  const contentBlocks = blocks[i].match(contentBlockMatcher);
+  const oppositionName = blocks[i].match(oppositionNameMatcher);
+
+  if (!roundName && !round) {
+    throw new Error("Round not found - unable to continue");
+  }
+
+  // New round
+  if (roundName) {
+    round = roundName[0]
+      .trim()
+      .replace("\r", "")
+      .replace("\n", "")
+      .replace(/=/g, "");
+  }
+
+  missions.push({
+    roundName: round,
+    missionName,
+    contentBlocks,
+    oppositionName
+  });
+}
+
+console.log(missions);
+document.write("<pre>" + WorldTourRaw + "</pre>");
